3279c8ef380e3b76dfa93be083a7ca00dab46c9c
Abhishek-Maurya2 <208akmaurya@hmail.com>
2024-01-03 01:38:11 +0530

    search done

--- Screens/Chat/AddChat.jsx
+++ Screens/Chat/AddChat.jsx
@@ -6,43 +6,48 @@ import {
   TextInput,
   Pressable,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Feather } from "react-native-vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
-import { setDoc, collection, addDoc, doc } from "firebase/firestore";
-import { firestoreDB } from "./../../Auth/FirebaseConfig"
+import { FirestoreDB, firestoreDB } from "./../../Auth/FirebaseConfig";
+import { getDocs, query, where, collection, orderBy, startAt, endAt } from "firebase/firestore";
 
 const AddChat = () => {
-//   const navigation = useNavigation();
-//   const user = useSelector((state) => state.userAuth.user);
-//   console.log("Add Chat : ", user);
-const navigation = useNavigation();
+  const navigation = useNavigation();
+
   const [showSearch, setShowSearch] = useState(false);
   const [addChat, setAddChat] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    if (addChat) {
+      search();
+    }
+  }, [addChat]);
+
   const OnSearch = () => {
     setShowSearch(true);
   };
 
-//   const createNewChat = async () => {
-//     let id = `${Date.now()}`;
-//     const _doc = {
-//       _id: id,
-//       user: user,
-//       chatName: addChat,
-//     };
-//     if (addChat !== "") {
-//       setDoc(doc(firestoreDB, "chats", id), _doc)
-//         .then(() => {
-//           setAddChat("");
-//           navigation.replace("Home");
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//           alert("Error : ", err);
-//         });
-//     }
-//   };
+ const search = async () => {
+   const q = query(
+     collection(FirestoreDB, "users"),
+     orderBy("PhoneNumber"),
+     startAt(addChat),
+     endAt(`${addChat}\uf8ff`)
+   );
+   const querySnapshot = await getDocs(q);
+   const results = [];
+   querySnapshot.forEach((doc) => {
+     results.push(doc.data());
+     console.log(doc.data());
+   });
+   setSearchResults(results);
+ };
+
+
+
   return (
     <View style={styles.container}>
       {showSearch ? (
@@ -63,7 +68,7 @@ const navigation = useNavigation();
             />
           </View>
           <View style={styles.addCont}>
-            <Pressable style={styles.btnCont} onPress={() =>{}}>
+            <Pressable style={styles.btnCont} onPress={search}>
               <Feather
                 name="user-plus"
                 size={28}
@@ -72,6 +77,23 @@ const navigation = useNavigation();
               />
               <Text style={styles.add}>Add User</Text>
             </Pressable>
+            {searchResults.map((user, index) => (
+              <View key={index}>
+                {/* Replace 'name' and 'phoneNumber' with the actual field names in your Firestore documents */}
+                <Pressable onPress={() => navigation.navigate(ChatPage)}>
+                  <View style={styles.listItem}>
+                    <Image
+                      source={{ uri: user.profilePic }}
+                      style={styles.photos}
+                    />
+                    <View style={styles.metaData}>
+                      <Text style={styles.name}>{user.fullName}</Text>
+                      <Text style={styles.time}>{user.PhoneNumber}</Text>
+                    </View>
+                  </View>
+                </Pressable>
+              </View>
+            ))}
           </View>
         </View>
       ) : (
@@ -94,7 +116,10 @@ const navigation = useNavigation();
               color="white"
               onPress={OnSearch}
             />
-            <Image style={styles.profile} source={require("./../../assets/images/1.png")} />
+            <Image
+              style={styles.profile}
+              source={require("./../../assets/images/1.png")}
+            />
           </View>
         </View>
       )}
@@ -176,4 +201,31 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     marginLeft: 10,
   },
+  listItem: {
+    flexDirection: "row",
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  photos: {
+    width: 45,
+    height: 45,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  metaData: {
+    borderBottomWidth: 0,
+    marginLeft: 10,
+    marginBottom: 10,
+    paddingBottom: 10,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  time: {
+    fontSize: 12,
+    color: "#666",
+  },
+  
 });
